acf000ee14a5a9608014791039afc5615d9b7e08
notes-a-bit <ssarwarjahan@gmail.com>
2024-08-13 14:03:41 +0600

    Added admin functionality for event and registration models

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Event, Registration
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'time', 'location', 'available_slots')
+    search_fields = ('title', 'description')
+
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'registration_date')
+    list_filter = ('event',)
+    search_fields = ('user__username', 'event__title')
